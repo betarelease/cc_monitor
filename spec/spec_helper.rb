12565b1600da4c81501462c820940bc9ed683e4a
betarelease <sudhindra.r.rao@gmail.com>
2009-01-09 23:39:11 -0600

    Sudhindra : froze rack gems, fixed typo in paths

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,3 @@
+require File.join(File.expand_path(File.dirname(__FILE__)), "..", "vendor/rack-0.4.0/lib/rack")
 require File.join(File.expand_path(File.dirname(__FILE__)), "..", "vendor/ramaze-2008.11/lib/ramaze")
 require 'ramaze/spec/helper'
--- start.rb
+++ start.rb
@@ -1,3 +1,4 @@
+require File.join(File.expand_path(File.dirname(__FILE__)), "./vendor/rack-0.4.0/lib/rack")
 require File.join(File.expand_path(File.dirname(__FILE__)), "./vendor/ramaze-2008.11/lib/ramaze")
 
 # require all controllers and models
